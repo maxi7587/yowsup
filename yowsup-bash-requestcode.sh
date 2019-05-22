7b13aa8d6871265bf2954da56b311b41b7d69ce3
Maxi <maxi7587@hotmail.com>
2019-05-22 01:17:41 -0300

    testing

--- yowsup-bash-requestcode.sh
+++ yowsup-bash-requestcode.sh
@@ -3,20 +3,37 @@
 # Should run the following line (using example data):
 # python yowsup-cli registration --requestcode sms --config-phone 46766921516 --config-cc 46 --config-mcc 240 --config-mnc 01
 
-default_phone_number=46766921516
-default_cc_number=46
-default_mcc_number=240
-default_mnc_number=01
+phone_number=542604268467
+cc_number=54
+mcc_number=722
+mnc_number=320
 
-read -p "Enter your number (leave blank to use stored default value): " phone_number
+read -p "Enter your number (leave blank to use stored default value): " new_phone_number
 
-if [ phone_number ]
+test "$new_phone_number" == ""
+
+echo $?
+
+if $?
 then
-    read -p "Enter your CC (CountyCode) number (leave blank to use stored default value): " cc_number
-    read -p "Enter your MCC (MobileCountyCode) number (leave blank to use stored default value): " mcc_number
-    read -p "Enter your MNC (MobileNetworkCode) number (leave blank to use stored default value): " mnc_number
-    yowsup-cli registration --requestcode sms --config-phone $phone_number --config-cc $cc_number --config-mcc $mcc_number --config-mnc $mnc_number
-else
-    yowsup-cli registration --register 123456 --config-phone $default_phone_number
-    yowsup-cli registration --requestcode sms --config-phone $default_phone_number --config-cc $default_cc_number --config-mcc $default_mcc_number --config-mnc $default_mnc_number
+    phone_number=new_phone_number
+    read -p "Enter your CC (CountyCode) number (leave blank to use stored
+    default value): " cc_number
+    read -p "Enter your MCC (MobileCountyCode) number (leave blank to use stored
+    default value): " mcc_number
+    read -p "Enter your MNC (MobileNetworkCode) number (leave blank to use
+    stored default value): " mnc_number
 fi
+
+echo "Will request a verification code with the following data:"
+echo "- cc $cc_number"
+echo "- mcc $mcc_number"
+echo "- mnc $mnc_number"
+echo "- number $phone_number"
+
+#yowsup-cli registration --requestcode sms --config-phone $phone_number --config-cc $cc_number --config-mcc $mcc_number --config-mnc $mnc_number
+
+echo -n "Enter the verification code: "
+read verification_code
+
+yowsup-cli registration --register $verification_code --config-phone $phone_number
